805eff39e6ee48cfe4d85ce7542fda3de3269c57
Pedro Conde <pconde007@gmail.com>
2023-08-17 12:08:14 +0100

    Commit desde Visual Studio Code

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { environments } from 'src/environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -28,5 +28,22 @@ export class HeroesService {
 
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if ( !hero.id) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( err => of(false)),
+        map( resp => true)
+      );
+  }
+
 }
-;
+
